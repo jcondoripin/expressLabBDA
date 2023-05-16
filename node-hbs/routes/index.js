723f9dc84587e77f363ba822e19ba4351ce8d20f
jcondoripin <jcondoripin@unsa.edu.pe>
2023-05-16 12:00:50 -0500

    app04 final version

--- node-hbs/routes/index.js
+++ node-hbs/routes/index.js
@@ -3,9 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('usuario/index', { title: 'Express-usuario' });
-  res.render('producto/index', { title: 'Express-producto' });
-  res.render('empleado/index', { title: 'Express-producto' });
+  res.render('index', { title: 'Express-mantenimiento' });
 });
 
 module.exports = router;
