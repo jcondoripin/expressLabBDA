a45352ed6bc97a9be75431ee0bfa84f0b47ce7ee
jcondoripin <jcondoripin@unsa.edu.pe>
2023-05-16 12:04:49 -0500

    final version of bd

--- db.js
+++ db.js
@@ -1,7 +1,18 @@
 conn = new Mongo();
 db = conn.getDB("myappdb");
 
-db.Usuario.insert(
+db.usuarios.insert(
   [
    {nombre: 'Jaime', apellido: 'Farfan', email: 'jfarfan@abc.com', estado:'A', created_at: new Date('01/23/2020')}
- ]);
+  ]
+);
+db.productos.insert(
+  [
+    {nombre: 'Producto1', costo: 12, descripcion: 'descp1', stock: 50}
+  ]
+);
+db.empleados.insert(
+  [
+    {nombre: 'emp1', apellido: 'lastemp1', sueldo: 12, cargo: 'gerente', dni: '61257146', telefono: '999999999'}
+  ]
+);
